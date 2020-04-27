131db1f8645a1ff79947c4f143754963c868387a
jl0822 <jluicanada@gmail.com>
2020-04-27 14:45:04 +0900

    add styling and products, rails generated store

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,32 @@
 Rails.application.routes.draw do
-  resources :products
+  # PWA routes
+  get '/service_worker.js' => 'service_worker#service_worker'
+  get '/manifest.json' => 'service_worker#manifest'
+  
+  get 'admin' => 'admin#index'
+
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+  resources :users
+  resources :products do
+    get :who_bought, on: :member
+  end
+
+  resources :support_requests, only: [:index, :update]
+
+  scope '(:locale)' do
+    # QR code test page
+    get 'qr-code' => 'qr_codes#index'
+
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index', via: :all
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
